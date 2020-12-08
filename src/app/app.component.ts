43d5b6ce314bf17b007d7be345c5396dab7b3a73
VANKE\chenql07 <249370878@qq.com>
2020-12-08 15:35:00 +0800

    feat: add hot code push demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +18,16 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    document.addEventListener('chcp_updateIsReadyToInstall', this.eventCallback.bind(this), false);
+  }
+
+  eventCallback() {
+    alert('chcp_updateIsReadyToInstall enter');
+    // const error = eventData.details.error;
+    // if (error) {
+    //   console.log('Error with code: ' + error.code);
+    //   console.log('Description: ' + error.description);
+    // }
   }
 }
 
